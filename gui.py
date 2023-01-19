1d5a7eb4bdc843e2f489887285882ace0e67ffe1
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-19 13:33:56 -0700

    Implemented Add Todo Feature

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-
+from helper_functions import get_todos, write_todos
 import PySimpleGUI as psg
 
 label = psg.Text("Enter a to-do")
@@ -8,5 +8,18 @@ add_button = psg.Button("Add")
 window = psg.Window("To-Do App",
                     layout=[[label], [input_box, add_button]],
                     font=('Helvetica', 20))
-event, values = window.read()
+
+while True:
+    event, values = window.read()
+
+    if event == 'Add':
+
+        new_to_do = f"{values['todo']}\n"
+        to_dos = get_todos()
+        to_dos.append(new_to_do)
+        write_todos(to_dos)
+
+    elif psg.WIN_CLOSE:
+        break
+
 window.close()
